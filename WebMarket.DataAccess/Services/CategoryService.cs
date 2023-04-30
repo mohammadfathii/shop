429cee314edf244ebccf0aae2fa0c69d95652afd
Mohammad <m_fathi65@yahoo.com>
2023-04-30 20:23:32 +0430

    CategoryService instace of Repository Category

---
+++ WebMarket.DataAccess/Services/CategoryService.cs
@@ -0,0 +1,41 @@
+using WebMarket.Models;
+using WebMarket.DataAccess.Data;
+using System.Linq.Expressions;
+
+namespace WebMarket.DataAccess.Services
+{
+    public class CategoryService{
+        public WebMarket_DB WebMarket_DB;
+        public CategoryService(WebMarket_DB webMarket_DB){
+            WebMarket_DB = webMarket_DB;
+        }
+
+        public IEnumerable<Category> GetAll(){
+            IEnumerable<Category> categories = WebMarket_DB.Categories;
+            return categories;
+        }
+
+        public Category GetFirstOrDefault(Expression<Func<Category,bool>> filter){
+            IQueryable<Category> category = WebMarket_DB.Categories;
+            category = category.Where(filter);
+            return category.FirstOrDefault();
+        }
+
+        public void Add(Category category){
+            WebMarket_DB.Categories.Add(category);
+        }
+
+        public void Update(Category category){
+            WebMarket_DB.Categories.Update(category);
+        }
+
+        public void Remvoe(Category category){
+            WebMarket_DB.Categories.Remove(category);
+        }
+
+        public void RemoveRange(IEnumerable<Category> categories){
+            WebMarket_DB.Categories.RemoveRange(categories);
+        }
+
+    }
+}
