97b07844317adeb5d45c9854962131fd14583542
Mohammad <m_fathi65@yahoo.com>
2023-04-30 22:33:27 +0430

    added migration of product and update database

--- WebMarket.DataAccess/Data/WebMarket_DB.cs
+++ WebMarket.DataAccess/Data/WebMarket_DB.cs
@@ -11,5 +11,6 @@ namespace WebMarket.DataAccess.Data
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<CoverType> CoverTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
--- WebMarket.DataAccess/Migrations/20230430175846_AddTableProduct.Designer.cs
+++ WebMarket.DataAccess/Migrations/20230430175846_AddTableProduct.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebMarket.DataAccess.Data;
 
@@ -11,9 +12,11 @@ using WebMarket.DataAccess.Data;
 namespace WebMarket.DataAccess.Migrations
 {
     [DbContext(typeof(WebMarket_DB))]
-    partial class WebMarket_DBModelSnapshot : ModelSnapshot
+    [Migration("20230430175846_AddTableProduct")]
+    partial class AddTableProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
