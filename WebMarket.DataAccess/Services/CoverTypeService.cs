753521c3d311f22a7705f2bad4f6a34a6fac36a6
Mohammad <m_fathi65@yahoo.com>
2023-04-30 20:16:54 +0430

    added CoverTypeService Instace of CoverType Repository

--- WebMarket.DataAccess/Services/CoverTypeService.cs
+++ WebMarket.DataAccess/Services/CoverTypeService.cs
@@ -16,10 +16,31 @@ namespace WebMarket.DataAccess.Services
         }
 
         public CoverType GetFirstOrDefault(Expression<Func<CoverType,bool>> filter){
-            CoverType coverType = WebMarket_DB.CoverTypes.FirstOrDefault(filter);
-            return coverType;
+            // !! use IQuerable Type for using where method to search
+            IQueryable<CoverType> coverType = WebMarket_DB.CoverTypes;
+            coverType = coverType.Where(filter);
+            return coverType.FirstOrDefault();
         }
 
+        public void Add(CoverType coverType){
+            WebMarket_DB.CoverTypes.Add(coverType);
+        }
+
+        public void Update(CoverType coverType){
+            WebMarket_DB.CoverTypes.Update(coverType);
+        }
+
+        public void Remove(CoverType coverType){
+            WebMarket_DB.Remove(coverType);
+        }
+
+        public void RemoveRange(ICollection<CoverType> coverTypes){
+            WebMarket_DB.RemoveRange(coverTypes);
+        }
+
+        public void Save(){
+            WebMarket_DB.SaveChanges();
+        }
 
     }
 }
